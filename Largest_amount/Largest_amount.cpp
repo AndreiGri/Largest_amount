90924625c66ea72f700a3a2e6c8f8a88156a9168
Andrei_Gridin <andreigridin918@gmail.com>
2024-06-03 20:21:09 +0500

    Добавьте файлы проекта.

---
+++ Largest_amount/Largest_amount.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int>vec = { -5, 2, 8, -4, -10, 12, 7, -3 };
+	std::vector<int>temp(vec.size(), 0);
+	int sum = 0;
+
+	for (int i = 0; i < vec.size(); i++)
+	{
+		sum += vec[i];
+	}
+
+	for (int i = 0; i < vec.size(); i++)
+	{
+		if (sum - vec[i] > sum)
+		{
+
+		}
+	}
+
+    return 0;
+}
